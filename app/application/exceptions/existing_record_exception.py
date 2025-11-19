de14679ab7712df089fb6d71aa0b57dc411d773f
Marcio Andre <marcio.levada@exctaworks.com.br>
2025-11-19 19:25:30 -0300

    1

--- app/application/exceptions/existing_record_exception.py
+++ app/application/exceptions/existing_record_exception.py
@@ -1,3 +1,5 @@
 class ExistingRecordException(Exception):
     """Exceção lançada quando um registro já existe no banco de dados."""
-    pass
+    def __init__(self, message: str = "Registro já existe"):
+        self.message = message
+        super().__init__(self.message)
--- app/application/exceptions/not_found_record_exception.py
+++ app/application/exceptions/not_found_record_exception.py
@@ -1,3 +1,5 @@
 class NotFoundRecordException(Exception):
     """Exceção lançada quando um registro não é encontrado."""
-    pass
+    def __init__(self, message: str = "Registro não encontrado"):
+        self.message = message
+        super().__init__(self.message)
